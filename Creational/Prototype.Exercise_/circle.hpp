10e80002dc922cf27145a455b73cb7b8f6711498
pi03k <pi03k87@gmail.com>
2017-11-09 11:09:58 +0100

    prototype exercise done

---
+++ Creational/Prototype.Exercise_/circle.hpp
@@ -0,0 +1,35 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "shape.hpp"
+
+namespace Drawing
+{
+  class Circle : public ShapeBase
+  {
+    int radius_;
+
+    public:
+    static constexpr const char* id = "Circle";
+
+    Circle(int x = 0, int y = 0, int r = 0);
+
+    int radius() const
+    {
+      return radius_;
+    }
+
+    void set_radius(int r)
+    {
+      radius_ = r;
+    }
+
+    void draw() const override;
+    std::unique_ptr<Shape> clone() const override
+    {
+      return std::make_unique<Circle>(*this);
+    }
+  };
+}
+
+#endif // CIRCLE_H
